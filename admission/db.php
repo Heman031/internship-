7d033166f27e1b04448399fd8c352ad00fc6f92e
HEMAN RAJ <hemanraj2713@gmail.com>
2026-02-10 22:56:28 +0530

    Delete admission directory
    delect

--- admission/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "root@123", "admission_db");
-
-if (!$conn) {
-    die("Database connection failed");
-}
-
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = mysqli_connect("localhost", "root", "root@123", "admission_db");
